eab045f3d1041b04a82628202470dbfedc158a40
Shayan Sarnevesht <42shayan42@gmail.com>
2019-05-16 22:14:01 -0700

    yeetus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("jdslj");
 
-        Grid grid = new Grid();
-        grid.printGrid();;
+        TwoPlayerGame game = new TwoPlayerGame();
+        game.startGame();
     }
 }
