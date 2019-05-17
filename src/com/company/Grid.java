6c21e8d3567f489468ba08d15b3bda706cce3a61
Shayan Sarnevesht <42shayan42@gmail.com>
2019-05-16 19:05:16 -0700

    Adding grid and square objects

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -1,9 +1,19 @@
 package com.company;
 
 public class Grid {
+    // height
+    public final int ROWS = 6;
+    // length
+    public final int COLS = 7;
+
+    public Square[][] grid = new Square[ROWS][COLS];
 
     public Grid(){
-
+        for(int i=0; i<ROWS; i++){
+            for(int j=0; j<COLS; j++){
+                grid[i][j] = new Square();
+            }
+        }
     }
 
 }
---
+++ src/com/company/Square.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Square {
+
+    public final int EMPTY = 0;
+    public final int X = 1;
+    public final int O = 1;
+
+    private int status;
+
+    // empty cell
+    public Square() {
+
+    }
+}
