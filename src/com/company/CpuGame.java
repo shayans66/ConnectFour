df76733298209fef8e2e5db9a547f655e270cd51
Shayan Sarnevesht <42shayan42@gmail.com>
2019-05-18 11:00:32 -0700

    Finished bot

--- src/com/company/CpuGame.java
+++ src/com/company/CpuGame.java
@@ -29,7 +29,6 @@ public class CpuGame {
 
             grid.printGrid();
 
-
             if(grid.isGridFull() || grid.isThereWin()){
                 break;
             }
@@ -59,10 +58,11 @@ public class CpuGame {
         System.out.print(obj + "'s turn! Enter a column from 1-7: ");
 
         int col = input.nextInt();
+        input.nextLine();
 
         System.out.println();
         while (col < 1 || col > 7) {
-            System.out.print("Out of bonds, try again: ");
+            System.out.print("Out of bounds, try again: ");
             col = input.nextInt();
             System.out.println();
         }
@@ -77,8 +77,41 @@ public class CpuGame {
 
     public void getBotTurn(int status){
         // 6 Rows, 7 Columns
-        System.out.print("It is the bot's turn! Press enter to continue. ");
+        String obj = "";
+        if(status == Square.X) obj = "X";
+        if(status == Square.O) obj = "O";
+        System.out.println("It is the bot's ( " + obj + " ) turn! Press enter to continue. ");
+
         input.nextLine();
+        //System.out.println("*");
+        //input.nextLine();
+        //System.out.println("**");
+
+
+
+
+        if( grid.hasNumInARow(status, 3)   ){
+            int[] arr = grid.getBotMove();
+            if( arr==null ){
+                int col = 1 + rand.nextInt(7);
+
+                while( grid.isColFull(col)){
+                    col = 1 + rand.nextInt(7);
+                }
+
+                grid.setCol(col, status);
+                return;
+            }
+            int row = arr[0];
+            int col = arr[1];
+
+            if(grid.canPieceBePutHere(row,col)){
+
+                grid.setCol(col,status);
+                return;
+            }
+        }
+
 
         int col = 1 + rand.nextInt(7);
 
@@ -87,5 +120,8 @@ public class CpuGame {
         }
 
         grid.setCol(col, status);
+    }
+    public void recordRandomBotMove(){
+
     }
 }
